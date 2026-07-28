680870945e214775ba342d8ee0f807d03c75a7b1
barryallen0015 <77632393+barryallen0015@users.noreply.github.com>
2026-07-28 21:01:18 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+  const userEmail = email.value.trim();
+  const userPassword = password.value.trim();
+
+  if (userEmail === "" || userPassword === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  console.log({
+    email: userEmail,
+    password: userPassword,
+  });
+
+  event.currentTarget.reset();
+});
