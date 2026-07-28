5f3c76f7fc957c954882b8690385cd1bd674d1bf
barryallen0015 <77632393+barryallen0015@users.noreply.github.com>
2026-07-28 21:02:33 +0300

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorButton = document.querySelector(".change-color");
+const colorText = document.querySelector(".color");
+
+changeColorButton.addEventListener("click", () => {
+  const color = getRandomHexColor();
+
+  document.body.style.backgroundColor = color;
+  colorText.textContent = color;
+});
